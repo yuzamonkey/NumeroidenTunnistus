6cedbbbaa09c1be84f7609d9288ba718eb3e5b03
Jusa Mankki <jusamankki@gmail.com>
2021-11-08 13:46:24 +0200

    sandboxing with mnist

--- documentation/worklog.md
+++ documentation/worklog.md
@@ -4,11 +4,11 @@
 
 Pvm       | h |  |
 -----------|------|--------|
-4.11.2021 | 1,5 | Alusta git repo, dokumentaatiota |
-6.11.2021 | 2,5 | Dokumentaatiota, lähteiden etsimistä ja lukemista, toteutuksen suunnittelua kynällä ja paperilla |
+4.11.2021 | 1.5 | Alusta git repo, dokumentaatiota |
+6.11.2021 | 2.5 | Dokumentaatiota, lähteiden etsimistä ja lukemista, toteutuksen suunnittelua kynällä ja paperilla |
 
 ## Viikko 2
 
 Pvm       | h |  |
 -----------|------|--------|
-8.11.2021 | 12- | Konfiguraatioita, MNIST-dataan tutustuminen ja datan lukeminen koodilla |
+8.11.2021 | 0.5, 1305-1345 | Konfiguraatioita, MNIST-dataan tutustuminen ja datan lukemista koodilla |
--- src/main.py
+++ src/main.py
@@ -1,5 +1,36 @@
+from mnist import MNIST
+
+
+class MNISTGETTER():
+    def __init__(self):
+        self._mndata = MNIST("./data")
+        self._mndata.gz = True
+
+    def get_data(self):
+        train_imgs, train_labels = self._mndata.load_training()
+        test_imgs, test_labels = self._mndata.load_testing()
+        return train_imgs, train_labels, test_imgs, test_labels
+
+
 def main():
-  print("HELLO CLASSIFIER")
+    train_imgs, train_labels, test_imgs, test_labels = MNISTGETTER().get_data()
+    print_image_and_result(train_imgs[0], train_labels[0])
+    print_image_and_result(test_imgs[0], test_labels[0])
+    print(len(train_imgs))
+    print(len(test_imgs))
+
+
+def print_image_and_result(img, result):
+    for i in range(len(img)):
+        to_print = ". "
+        if img[i] != 0:
+            to_print = "@ "
+        if (i+1) % 28 == 0:
+            print(to_print, end="\n")
+        else:
+            print(to_print, end="")
+    print("IS: ", result)
+
 
 if __name__ == "__main__":
-  main()
+    main()
