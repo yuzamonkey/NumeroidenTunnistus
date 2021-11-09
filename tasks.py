5b0d2bf48919616051e3e7b87256b2665b58aea1
Jusa Mankki <jusamankki@gmail.com>
2021-11-09 15:47:12 +0200

    add coverage

--- tasks.py
+++ tasks.py
@@ -19,3 +19,13 @@ def lint(ctx):
 @task
 def format(ctx):
     ctx.run("autopep8 --in-place --recursive src")
+
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src")
+
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html")
