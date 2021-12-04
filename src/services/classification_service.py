b76f1c2000672592c28059cc8cf1f5603303d2ea
Jusa Mankki <jusamankki@gmail.com>
2021-12-04 14:38:49 +0200

    add text to error image

--- src/services/classification_service.py
+++ src/services/classification_service.py
@@ -24,6 +24,9 @@ class ClassificationService:
         results.set_correct_count(correct_count)
         results.set_errors_count(errors_count)
         results.set_errors(errors)
-
+    
+    def get_label_from_test_data(self, idx):
+        labels = data_repository.get_testing_labels()
+        return labels[idx]
 
 classification_service = ClassificationService()
